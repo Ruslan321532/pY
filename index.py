04ee75a7dfd86548da1f58453fed8a419beb0495
Ruslan321532 <ruslanburalkiev8@gmail.com>
2023-03-04 18:42:04 +0600

    pY

---
+++ index.py
@@ -0,0 +1,51 @@
+# # def my_function():
+# #     print("Hello world")
+# # my_function()
+
+
+# def my_function(child3, child2, child1):
+#     print("The youngest child  is " + child3)
+
+
+# my_function(child1="Email", child2="tobias", child3="linus")
+
+# def my_function(country="Norway"):
+#     print("I am from " + country)
+
+
+# my_function("Sweden")
+# my_function("India")
+# my_function()
+# my_function("Brazil")
+
+# def my_function(food):
+#     for x in food:
+#         print(x)
+
+
+# fruits = ["apple", "banana", "cherry"]
+
+# my_function(fruits)
+
+
+# def my_function(x):
+#     return 5 * x
+
+
+# print(my_function(3))
+# print(my_function(5))
+# print(my_function(9))
+
+# thisset = {"apple", "banana", "cherry"}
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+# print(thisset)
+
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {1, 5, 7, 9, 3}
+# set3 = {True, False, False}
+
+# myset = {"apple", "banana", "cherry"}
+# print(type(myset))
